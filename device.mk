14938acc7d3946ea205b02886c8c55ec4f379911
davidevinavil <davidevinavil@gmail.com>
2016-10-30 15:15:21 +0100

    initial commit
    Change-Id: I472f761ffc641aa787609c93abbca5f24d3f3616

--- device.mk
+++ device.mk
@@ -269,5 +269,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
